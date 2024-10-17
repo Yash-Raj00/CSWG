03c9b14e961d223ecab212b427eab8d0665f3286
Praduman <praduman@maguresoftwares.com>
2024-10-17 18:34:11 +0530

    revert label

--- src/app/dashboard/runLogActions.js
+++ src/app/dashboard/runLogActions.js
@@ -4,7 +4,7 @@ import { revalidatePath } from "next/cache";
 import { SelectQuery } from "@/lib/common/db/pool";
 
 const selectAction = async (env) => {
-  const selectShippingJobRowsQueryAllTime = `select * from wip_configurations.spark_streaming_orsyp_run_log LIMIT 500`;
+  const selectShippingJobRowsQuery = `select * from wip_configurations.spark_streaming_orsyp_run_log LIMIT 500`;
   const selectShippingJobRowsQueryToday = `SELECT * FROM wip_configurations.spark_streaming_orsyp_run_log 
   WHERE created_date >= toTimestamp(toDate(now()))
   LIMIT 500
